55f2d7088b1d95419e9fa52e8b0790c22896ffcf
Jose Coelho <jose.coelho@skybettingandgaming.com>
2016-04-09 15:21:44 +0100

    testing

--- generator/rand.go
+++ generator/rand.go
@@ -1,9 +1,9 @@
 package generator
 
-func RandInt(min int, max int, c *Config) int {
+func RandInt(min, max int, c *Config) int {
 	return c.getRand().Intn(max-min) + min
 }
 
-func RandFloat(min float64, max float64, c *Config) float64 {
+func RandFloat(min, max float64, c *Config) float64 {
 	return c.getRand().Float64()*(max-min) + min
 }
--- main.go
+++ main.go
@@ -3,28 +3,35 @@
 package main
 
 import (
+	"flux/generator"
 	"log"
+	"math/rand"
 	"os"
-	"strings"
 	"text/template"
+	"time"
 )
+
+var config = &generator.Config{
+	Rand: rand.New(rand.NewSource((time.Now().Unix()))),
+}
 
 func main() {
 
 	funcMap := template.FuncMap{
-		"title": strings.Title,
+		"rand":      func(a, b int) int { return generator.RandInt(a, b, config) },
+		"randFloat": func(a, b float64) float64 { return generator.RandFloat(a, b, config) },
 	}
 	templateText := `
-    Output 0: {{title "adsa" }}
-    Output 1: {{title "asda asdad" "123131"}}
+    Output 1: "{{ randFloat 10.01 15.02 | printf "%.2f" }}"
 `
-	tmpl, err := template.New("titleTest").Funcs(funcMap).Parse(templateText)
-	if err != nil {
-		log.Fatalf("parsing: %s", err)
-	}
-	// Run the template to verify the output.
-	err = tmpl.Execute(os.Stdout, nil)
-	if err != nil {
-		log.Fatalf("execution: %s", err)
+	tmpl := template.Must(template.New("titleTest").Funcs(funcMap).Parse(templateText))
+
+	for {
+		// Run the template to verify the output.
+		err := tmpl.Execute(os.Stdout, nil)
+		if err != nil {
+			log.Fatalf("execution: %s", err)
+		}
+
 	}
 }
